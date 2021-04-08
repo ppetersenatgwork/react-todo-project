6754c0f60e311d3bedfa51a1cbdc5ebc2e89c058
Patrick <ppetersen@atgworks.com>
2021-04-08 14:35:46 -0400

    Update to use HashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer/TodoContainer"
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <TodoContainer />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 )
