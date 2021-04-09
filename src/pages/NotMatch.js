3a8a97daebf8d046d4c8acb825e97fb72b566977
Patrick <ppetersen@atgworks.com>
2021-04-09 13:14:59 -0400

    Updated 404 page display

--- src/pages/NotMatch.js
+++ src/pages/NotMatch.js
@@ -2,8 +2,11 @@ import React from "react"
 
 export default function NotMatch() {
     return (
-        <div>
-            <h3>No match for this page</h3>
+        <div style={{position: "absolute",
+            top: "25%",
+            left: "50%",
+            transform: "translate(-50%, -50%)"}}>
+            <h1>No match for this page</h1>
         </div>
     )
 }
